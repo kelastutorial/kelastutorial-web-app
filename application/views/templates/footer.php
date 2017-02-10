c28a861ea9a633aaad6f0401478427499d64bd6b
purwanto1337 <purwanto1337@gmail.com>
2017-02-10 17:02:01 +0000

    modify footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,19 +1,19 @@
         <div class="section footer">
             <div class="row footer-menu-wrapper">
-                <div class="col s6 m6 l3 grey darken-1 footer-box-wrapper" style="padding:35px 20px 50px 20px;">
+                <div class="col s6 m6 l3 footer-box-wrapper" style="padding:35px 20px 50px 20px;background-color:#f5f5f5;">
                     <span class="footer-menu">Kelas Tutorial</span><br/>
                     <a href="<?php echo site_url('site-terms'); ?>"><span class="footer-submenu">Syarat Ketentuan</span></a><br/>
                     <a href="<?php echo site_url('privacy-policy'); ?>"><span class="footer-submenu">Kebijakan Privasi</span></a><br/>
                     <a href="<?php echo site_url('contact'); ?>"><span class="footer-submenu">Hubungi Kami</span></a>  
                 </div>
-                <div class="col s6 m6 l3 grey darken-1 footer-box-wrapper" style="padding:35px 20px 50px 20px;">
+                <div class="col s6 m6 l3 footer-box-wrapper" style="padding:35px 20px 50px 20px;background-color:#f5f5f5;">
                     <span class="footer-menu">Dukungan</span><br/>
                     <a href="<?php echo site_url('program'); ?>"><span class="footer-submenu">Program</span></a><br/>
                     <a href="<?php echo site_url('partner'); ?>"><span class="footer-submenu">Partner</span></a><br/>
                     <a href="<?php echo site_url('reference'); ?>"><span class="footer-submenu">Referensi</span></a><br/>
                     <a href="<?php echo site_url('report-issues'); ?>"><span class="footer-submenu">Laporkan Isu</span></a>
                 </div>
-                <div class="col s6 m6 l3 grey darken-1 footer-box-wrapper" style="padding:35px 20px 50px 20px;">
+                <div class="col s6 m6 l3 footer-box-wrapper" style="padding:35px 20px 50px 20px;background-color:#f5f5f5;">
                     <span class="footer-menu">Tetap Terhubung</span><br/>
                     <a href="<?php echo site_url('event'); ?>"><span class="footer-submenu">Event</span></a><br/>
                     <a href="https://www.github.com/kelastutorial"><span class="footer-submenu">Github</span></a><br/>
@@ -21,7 +21,7 @@
                     <a href="https://www.instagram.com/kelastutorial"><span class="footer-submenu">Instagram</span></a><br/>
                     <a href="https://www.twitter.com/kelastutorial"><span class="footer-submenu">Twitter</span></a>  
                 </div>
-                <div class="col s6 m6 l3 grey darken-2 footer-box-wrapper" style="padding:35px 20px 50px 20px;">
+                <div class="col s6 m6 l3 grey darken-2 footer-box-wrapper" style="padding:35px 20px 50px 20px;background-color:#e0e0e0;">
                     <img style="width:90%;margin-top:15px;" src="<?php echo site_url('images/kelastutorial-logo-white.svg'); ?>"/><br>
                     <span style=" line-height: 20px;color:rgba(255,255,255,0.8);">Kelas Tutorial adalah situs edukasi dengan misi untuk menyediakan pendidikan gratis dengan perpustakaan tutorial dalam bahasa Indonesia.</span>
                 </div>
