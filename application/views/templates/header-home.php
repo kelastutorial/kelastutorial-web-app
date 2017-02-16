3bbdf3be98d58c957c88841bef9f485a1c6556d8
purwanto1337 <purwanto1337@gmail.com>
2017-02-16 15:49:46 +0000

    update title and meta description

--- application/views/templates/header-home.php
+++ application/views/templates/header-home.php
@@ -9,6 +9,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <meta name="author" content="Kelastutorial">
         <meta name="language" content="Indonesia">
+        <meta property="og:url" content="https://kelastutorial.com"/>
+        <meta property="og:title" content="Kelastutorial - Belajar cepat dan mudah"/>
+        <meta property="og:description" content="Kelastutorial adalah situs edukasi dengan misi untuk menyediakan pendidikan gratis dengan perpustakaan tutorial dalam bahasa Indonesia." />
+        <meta property="og:image" content="<?php echo site_url('images/kelastutorial-icon-large.png'); ?>" />
         <link rel="shortcut icon" href="<?php echo site_url('images/favicon.png'); ?>" type="image/x-icon" />
         <link rel="stylesheet" href="<?php echo site_url('assets/css/materialize.min.css'); ?>"/>
         <link rel="stylesheet" href="<?php echo site_url('assets/css/styles/atom-one-dark.css'); ?>"/>
@@ -17,6 +21,21 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono">
         <link rel="stylesheet" href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
         <link rel="stylesheet" href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'/>
+        <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "WebSite",
+            "url": "https://kelastutorial.com/",
+            "name": "Kelastutorial",
+            "description": "Kelas Tutorial adalah situs edukasi dengan misi untuk menyediakan pendidikan gratis dengan perpustakaan tutorial dalam bahasa Indonesia.",
+            "learningResourceType": "Tutorial",
+            "potentialAction": {
+              "@type": "SearchAction",
+              "target": "https://example.com/search?&q={query}",
+              "query-input": "required"
+            }
+        }
+        </script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
         <script type="text/javascript" src="<?php echo site_url('assets/js/materialize.min.js'); ?>"></script>
         <script type="text/javascript" src="<?php echo site_url('assets/js/highlight.pack.js'); ?>"></script>
