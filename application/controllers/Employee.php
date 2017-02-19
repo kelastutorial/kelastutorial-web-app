9f4af81fc2d9cd2a519fcd77269fbf477ba65923
purwanto1337 <purwanto1337@gmail.com>
2017-02-19 13:03:59 +0000

    add search feature

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -1,10 +1,12 @@
 <?php
-class Employee extends Controller {
-
- function Employee(){
-  parent::Controller();
-  $this->load->model('EmployeeModel');
- }
+class Employee extends CI_Controller {
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('employeeModel');
+        $this->load->helper('url_helper');
+      		$this->load->library('session');
+        $this->load->library('form_validation');
+    }
 
  public function index(){
   $search=  $this->input->post('search');
