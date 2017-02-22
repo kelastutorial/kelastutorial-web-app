8c60a4427bf8ca4c4e5a85a042a8b60b6968c8f2
purwanto1337 <purwanto1337@gmail.com>
2017-02-22 23:33:08 +0000

    update article font size

--- application/views/tutorials/git/adminUpdatePost.php
+++ application/views/tutorials/git/adminUpdatePost.php
@@ -6,7 +6,7 @@
     <label for="content">Content</label><br/>
     <textarea name="content" rows="50" cols="150"><?php echo $item['content']; ?></textarea><br/>
     <label for="meta_desc">Meta Description</label><br/>
-    <textarea name="meta_desc" rows="20"cols="150"></textarea><br/>
+    <textarea name="meta_desc" rows="20"cols="150"><?php echo $item['meta_desc']; ?></textarea><br/>
     <input type="hidden" name="published" value="<?php echo $item['published']; ?>" readonly="readonly">
     <input type="hidden" name="last_update" value="<?php echo date('Y-m-d'); ?>" readonly="readonly">
     <input type="submit" name="submit" value="submit"/><br/>
