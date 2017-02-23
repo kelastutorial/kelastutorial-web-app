393eade2121eb71c08d85c5d6a5fde9246fdcbd5
purwanto1337 <purwanto1337@gmail.com>
2017-02-23 17:28:14 +0000

    update about page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -5,8 +5,7 @@
                 
             </div>
         </div>
-        <div style="margin-top:0;height:300px;background-image: url('https://kelastutorial.com/images/about-kelastutorial-contact.jpg');background-color: #ffcdd2 ;background-size: cover;background-position:center;padding:200px 20px 0 20px;text-align:center;color:#fff;font-size:20px;">
+        <div style="margin-top:0;background-image: url('https://kelastutorial.com/images/about-kelastutorial-contact.jpg');background-color: #ffcdd2 ;background-size: cover;background-position:center;padding:50px 20px 50px 20px;text-align:center;color:#fff;font-size:15px;">
             <h3>Hubungi Kami</h3>
             <span>Jika Anda mempunyai pertanyaan, kami mempunyai jawaban.<br/>Kami akan selalu membantu Anda. Hubungi kami kapan saja.</span>
             <a href="mailto:support@kelastutorial.com" style="padding:20px 30px;background-color:#fff;color:#d32f2f;text-decoration:none;">HUBUNGI KAMI SEKARANG</a>
-        </div>
