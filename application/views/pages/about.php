8a014a1e5bd9a234c4e3ee05f2b3d786038bb966
purwanto1337 <purwanto1337@gmail.com>
2017-02-23 17:36:12 +0000

    update about page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,12 +1,12 @@
 
         <div style="margin-top:0;height:500px;background-image: url('https://kelastutorial.com/images/about-kelastutorial-mission.jpg');background-color: #000;background-size: cover;background-position:center;padding:200px 20px 0 20px;text-align:center;color:#fff;font-size:35px;font-weight:500"><span>Misi kami adalah menyediakan pustaka tutorial<br/> dalam Bahasa Indonesia yang mudah diakses dan dipergunakan<br/>seraca gratis oleh semua orang. #PengetahuanAdalahGratis</span></div>
-        <div class="container">
+        <div class="container" style="height:600px;">
             <div class="row">
                 
             </div>
         </div>
-        <div style="margin-top:0;height:auto;background-image: url('https://kelastutorial.com/images/about-kelastutorial-contact.jpg');background-size:cover;background-color: #ffcdd2 ;background-position:center;padding:40px 20px 80px 20px;text-align:center;color:#fff;font-size:15px;">
+        <div style="margin-top:0;height:auto;background-image: url('https://kelastutorial.com/images/about-kelastutorial-contact.jpg');background-size:cover;background-color: #ffcdd2 ;background-position:center;padding:40px 20px 80px 20px;text-align:center;color:#fff;font-size:18px;">
             <h4>Hubungi Kami</h4>
             <span>Jika Anda mempunyai pertanyaan, kami mempunyai jawaban.<br/>Kami akan selalu membantu Anda. Hubungi kami kapan saja.</span><br/><br/>
-            <a href="mailto:support@kelastutorial.com" class=" white waves-effect waves-light btn-large red-text">HUBUNGI KAMI SEKARANG</a></a>
+            <a href="mailto:support@kelastutorial.com" class=" white waves-effect waves-light btn-large red-text hoverable">HUBUNGI KAMI SEKARANG</a></a>
         </div>
