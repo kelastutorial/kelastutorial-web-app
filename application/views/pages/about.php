6adce4623b681548c0e008e87a15a6c0064b183e
purwanto1337 <purwanto1337@gmail.com>
2017-02-23 17:38:29 +0000

    update about page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,8 +1,8 @@
 
-        <div style="margin-top:0;height:500px;background-image: url('https://kelastutorial.com/images/about-kelastutorial-mission.jpg');background-color: #000;background-size: cover;background-position:center;padding:200px 20px 0 20px;text-align:center;color:#fff;font-size:35px;font-weight:500"><span>Misi kami adalah menyediakan pustaka tutorial<br/> dalam Bahasa Indonesia yang mudah diakses dan dipergunakan<br/>seraca gratis oleh semua orang. #PengetahuanAdalahGratis</span></div>
-        <div class="container" style="height:600px;">
+        <div style="margin-top:0;height:auto;background-image: url('https://kelastutorial.com/images/about-kelastutorial-mission.jpg');background-color: #000;background-size: cover;background-position:center;padding:200px 20px 200px 20px;text-align:center;color:#fff;font-size:35px;font-weight:500"><span>Misi kami adalah menyediakan pustaka tutorial<br/> dalam Bahasa Indonesia yang mudah diakses dan dipergunakan<br/>seraca gratis oleh semua orang. #PengetahuanAdalahGratis</span></div>
+        <div class="container" style="height:600px;text-align:center">
             <div class="row">
-                
+                <p>Kelastutorial<br/>Kelas Tutorial adalah situs edukasi dengan misi untuk menyediakan pendidikan gratis dengan perpustakaan tutorial dalam bahasa Indonesia.</p>
             </div>
         </div>
         <div style="margin-top:0;height:auto;background-image: url('https://kelastutorial.com/images/about-kelastutorial-contact.jpg');background-size:cover;background-color: #ffcdd2 ;background-position:center;padding:40px 20px 75px 20px;text-align:center;color:#fff;font-size:18px;">
