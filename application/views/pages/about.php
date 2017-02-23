289ca38d1ba7c144ecb2d0a614ab4d2563864d0c
purwanto1337 <purwanto1337@gmail.com>
2017-02-23 17:25:09 +0000

    update about page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,2 +1,12 @@
 
-        <div style="margin-top:0;height:500px;background-image: url('https://kelastutorial.com/images/about-kelastutorial-mission.jpg');background-color: #000;background-size: cover;background-position:center;padding:200px 20px 0 20px;text-align:center;color:#fff;font-size:35px;font-weight:700"><span>Misi kami adalah untuk menyediakan pustaka tutorial<br/> dalam Bahasa Indonesia yang mudah diakses dan dipergunakan<br/>seraca gratis oleh semua orang.</span></div>
+        <div style="margin-top:0;height:500px;background-image: url('https://kelastutorial.com/images/about-kelastutorial-mission.jpg');background-color: #000;background-size: cover;background-position:center;padding:200px 20px 0 20px;text-align:center;color:#fff;font-size:35px;font-weight:500"><span>Misi kami adalah menyediakan pustaka tutorial<br/> dalam Bahasa Indonesia yang mudah diakses dan dipergunakan<br/>seraca gratis oleh semua orang. #PengetahuanAdalahGratis</span></div>
+        <div class="container">
+            <div class="row">
+                
+            </div>
+        </div>
+        <div style="margin-top:0;height:300px;background-image: url('https://kelastutorial.com/images/about-kelastutorial-contact.jpg');background-color: #ffcdd2 ;background-size: cover;background-position:center;padding:200px 20px 0 20px;text-align:center;color:#fff;font-size:20px;">
+            <h3>Hubungi Kami</h3>
+            <span>Jika Anda mempunyai pertanyaan, kami mempunyai jawaban.<br/>Kami akan selalu membantu Anda. Hubungi kami kapan saja.</span>
+            <a href="mailto:support@kelastutorial.com" style="padding:20px 30px;background-color:#fff;color:#d32f2f;text-decoration:none;">HUBUNGI KAMI SEKARANG</a>
+        </div>
