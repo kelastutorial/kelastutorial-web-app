61630ee97597be31ef3d3be1d04b75ac75cf9fac
purwanto1337 <purwanto1337@gmail.com>
2017-02-23 16:44:56 +0000

    update about page

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,2 +1,2 @@
 
-        <div style="margin-top:0;height:500px;background-image: url("https://kelastutorial.com/images/about-kelastutorial-mission.jpg");background-color: #000;background-size: cover;background-position:center;"><h2>Misi Kami adalah untuk menyediakan tutorial Berbahasa Indonesia secara gratis.</h2></div>
+        <div style="margin-top:0;height:500px;background-image: url('https://kelastutorial.com/images/about-kelastutorial-mission.jpg');background-color: #000;background-size: cover;background-position:center;padding:100px 20px 0 20px;text-align:center;color:#fff;"><h2>Misi Kami adalah untuk menyediakan tutorial Berbahasa Indonesia secara gratis.</h2></div>
