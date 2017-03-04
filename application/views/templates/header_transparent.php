822352cf2fca5bf27c73fba7baf9cd8368b3b350
purwanto1337 <purwanto1337@gmail.com>
2017-03-04 10:26:50 +0000

    update transparent header

--- application/views/templates/header_transparent.php
+++ application/views/templates/header_transparent.php
@@ -54,13 +54,7 @@
             });
             $('#whitenav').hide();
             $('#transparentnav').hide();
-            $(window).scroll(function () {
-            if ($(window).scrollTop() >= 50) {
-            $('#whitenav').show();
-            } else {
-            $('#transparentnav').show()
-            }
-            });
+            
             
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
