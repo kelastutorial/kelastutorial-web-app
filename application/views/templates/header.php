a386fc09e7cbe1b55a82965c56ec8e56699b1813
purwanto1337 <purwanto1337@gmail.com>
2017-03-06 18:45:18 +0000

    update html tutorial

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -53,6 +53,18 @@
                 });
             }); 
             $('.collapsible').collapsible();
+            $('a[href^="#"]').on('click',function (e) {
+        	    e.preventDefault();
+        
+        	    var target = this.hash;
+        	    var $target = $(target);
+        
+        	    $('html, body').stop().animate({
+        	        'scrollTop': $target.offset().top
+        	    }, 1100, 'swing', function () {
+        	        window.location.hash = target;
+        	    });
+        	});
             
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
