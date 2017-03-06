85800488a7efa9668ad388e978aa2a0a109d163c
purwanto1337 <purwanto1337@gmail.com>
2017-03-06 17:45:53 +0000

    update html tutorial

--- application/views/tutorials/html/index.php
+++ application/views/tutorials/html/index.php
@@ -1,33 +1,51 @@
-<div style="background-color:#fafbfc;t">
-    <div class="container center" style="padding:180px 0 130px 0;">
-        <span class="tutorials-index-category">PENGEMBANGAN WEB</span>
-        <h3 class="tutorials-index-head-three">Tutorial HTML</h3>
-        <p>HTML and CSS are markup languages and the building blocks that make up the web.</p>
-    </div>
-</div>
-<div class="container" style="padding:80px 0 80px 0">
-    <div class="row">
-        <div class="col s12 m6 l6" style="padding:0 100px 0 0;">
-            <h4 class="tutorials-index-head-four" style="color:#E44D26;">Tentang Bahasa HTML</h4>
-            <p>HTML and CSS are markup languages and the building blocks that make up the web. This course is called "Not your Typical Intro" because it does not follow the usual pattern of other courses and tutorials that you find on the Internet. Usually HTML and CSS are taught with a focus on language syntax. But knowing syntax alone does not enable you to create a webpage from a design. You need to know where to start and how to approach the task, in other words - you need to learn how to think like a front-end developer.</p>
-            <div class="row" style="margin-top:70px;">
-                <p>Pelajari HTML sekarang dan mulai membuat website yang berguna.</p>
-                <div class="col s6 m6 l6">
-                    <a width="100%" class="waves-effect waves-light btn-large  deep-orange darken-1"><i class="material-icons right">school</i>pelajari</a>
+        
+        <div style="background-color:#fafbfc;t">
+            <div class="container center" style="padding:180px 0 130px 0;">
+                <span class="tutorials-index-category">PENGEMBANGAN WEB</span>
+                <h3 class="tutorials-index-head-three">Tutorial HTML</h3>
+                <p>HTML and CSS are markup languages and the building blocks that make up the web.</p>
+            </div>
+        </div>
+        <div class="container" style="padding:80px 0 80px 0">
+            <div class="row">
+                <div class="col s12 m6 l6" style="padding:0 100px 0 0;">
+                    <h4 class="tutorials-index-head-four" style="color:#E44D26;">Tentang Bahasa HTML</h4>
+                    <p>HTML and CSS are markup languages and the building blocks that make up the web. This course is called "Not your Typical Intro" because it does not follow the usual pattern of other courses and tutorials that you find on the Internet. Usually HTML and CSS are taught with a focus on language syntax. But knowing syntax alone does not enable you to create a webpage from a design. You need to know where to start and how to approach the task, in other words - you need to learn how to think like a front-end developer.</p>
+                    <div class="row" style="margin-top:70px;">
+                        <p style="margin-left: 11px;color: #777;font-style: italic;font-size: 14px;">Pelajari HTML sekarang dan mulai membuat website yang berguna.</p>
+                        <div class="col s6 m6 l6">
+                            <a style="width:100%" class="waves-effect waves-light btn-large  deep-orange darken-1"><i class="material-icons right">school</i>pelajari</a>
+                        </div>
+                        <div class="col s6 m6 l6">
+                            <a style="width:100%" class="waves-effect waves-light btn-large grey lighten-1">lihat materi</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col s6 m6 l6">
-                    <a width="100%" class="waves-effect waves-light btn-large grey lighten-1">lihat materi</a>
+                <div class="col s12 m6 l6">
+                    <div class="center" style="padding:30px 0 30px 0;">
+                        <img style="width:60%;" alt="logo html" title="logo html" src="https://kelastutorial.com/images/html-logo.png"/>
+                    </div>
+                    <span class="tutorials-index-category">LEVEL</span><br/>
+                    <span style="font-weight:600;color:#444;">PEMULA</span><br/><br/>
+                    <span class="tutorials-index-category">KLASIFIKASI</span><br/>
+                    <span style="font-weight:600;color:#444;">PENGEMBANGAN WEB</span>
                 </div>
             </div>
         </div>
-        <div class="col s12 m6 l6">
-            <div class="center" style="padding:30px 0 30px 0;">
-                <img style="width:60%;" alt="logo html" title="logo html" src="https://kelastutorial.com/images/html-logo.png"/>
-            </div>
-            <span class="tutorials-index-category">LEVEL</span><br/>
-            <span style="font-weight:600;color:#444;">PEMULA</span><br/><br/>
-            <span class="tutorials-index-category">KLASIFIKASI</span><br/>
-            <span style="font-weight:600;color:#444;">PENGEMBANGAN WEB</span>
+        <div class="container" style="background-color:#F4F4F6;padding:130px 0 130px 0">
+            <h4 class="tutorials-index-head-four" style="color:#E44D26;">Apa yang akan Anda pelajari ?</h4>
+            <ul class="collapsible" data-collapsible="accordion">
+                <li>
+                  <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+                  <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                </li>
+                <li>
+                  <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+                  <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                </li>
+                <li>
+                  <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+                  <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                </li>
+              </ul>
         </div>
-    </div>
-</div>
