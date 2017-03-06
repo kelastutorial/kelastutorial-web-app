e097f03a29e284a7c8a23d9c096341581dfb2f4c
purwanto1337 <purwanto1337@gmail.com>
2017-03-06 13:44:02 +0000

    update html tutorial

--- application/views/tutorials/html/index.php
+++ application/views/tutorials/html/index.php
@@ -1,1 +1,21 @@
-test
+<div class="container">
+    <div class="row">
+        <div class="col s12 m6 l6">
+            <span>Hyper Text Markup Language</span>
+        </div>
+        <div class="col s12 m6 l6">
+            
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col s12 m6 l6">
+            <h4>Tentang Bahasa HTML</h4>
+            <p>HTML and CSS are markup languages and the building blocks that make up the web. This course is called "Not your Typical Intro" because it does not follow the usual pattern of other courses and tutorials that you find on the Internet. Usually HTML and CSS are taught with a focus on language syntax. But knowing syntax alone does not enable you to create a webpage from a design. You need to know where to start and how to approach the task, in other words - you need to learn how to think like a front-end developer.</p>
+        </div>
+        <div class="col s12 m6 l6">
+            df
+        </div>
+    </div>
+</div>
