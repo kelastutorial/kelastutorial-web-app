6dceee75a992ec6d3b7cf0803ae5b6f345714bf1
purwanto1337 <purwanto1337@gmail.com>
2017-03-06 15:32:24 +0000

    update html tutorial

--- application/views/tutorials/html/index.php
+++ application/views/tutorials/html/index.php
@@ -7,18 +7,18 @@
 </div>
 <div class="container" style="padding:80px 0 80px 0">
     <div class="row">
-        <div class="col s12 m6 l6" style="padding:0 30px 0 0;">
+        <div class="col s12 m6 l6" style="padding:0 100px 0 0;">
             <h4 class="tutorials-index-head-four" style="color:#E44D26;">Tentang Bahasa HTML</h4>
             <p>HTML and CSS are markup languages and the building blocks that make up the web. This course is called "Not your Typical Intro" because it does not follow the usual pattern of other courses and tutorials that you find on the Internet. Usually HTML and CSS are taught with a focus on language syntax. But knowing syntax alone does not enable you to create a webpage from a design. You need to know where to start and how to approach the task, in other words - you need to learn how to think like a front-end developer.</p>
         </div>
         <div class="col s12 m6 l6">
-            <div class="center">
-                <img style="width:70%;" src="https://kelastutorial.com/images/html-logo.png"/>
+            <div class="center" style="padding:30px 0 30px 0;">
+                <img style="width:60%;" src="https://kelastutorial.com/images/html-logo.png"/>
             </div>
             <span class="tutorials-index-category">LEVEL</span><br/>
-            <span class="tutorials-index-category bolder">PEMULA</span><br/><br/>
+            <span>PEMULA</span><br/><br/>
             <span class="tutorials-index-category">KLASIFIKASI</span><br/>
-            <span class="tutorials-index-category bold">PENGEMBANGAN WEB</span>
+            <span>PENGEMBANGAN WEB</span>
         </div>
     </div>
 </div>
