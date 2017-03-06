36f96e37c303b64c0ffbc752f82cd0a5403c7cf4
purwanto1337 <purwanto1337@gmail.com>
2017-03-06 16:21:14 +0000

    update html tutorial

--- application/views/tutorials/html/index.php
+++ application/views/tutorials/html/index.php
@@ -10,15 +10,23 @@
         <div class="col s12 m6 l6" style="padding:0 100px 0 0;">
             <h4 class="tutorials-index-head-four" style="color:#E44D26;">Tentang Bahasa HTML</h4>
             <p>HTML and CSS are markup languages and the building blocks that make up the web. This course is called "Not your Typical Intro" because it does not follow the usual pattern of other courses and tutorials that you find on the Internet. Usually HTML and CSS are taught with a focus on language syntax. But knowing syntax alone does not enable you to create a webpage from a design. You need to know where to start and how to approach the task, in other words - you need to learn how to think like a front-end developer.</p>
+            <div class="row">
+                <div class="col s6 m6 l6">
+                    <a class="waves-effect waves-light btn-large  deep-orange darken-1"><i class="material-icons right">school</i>pelajari serakang</a>
+                </div>
+                <div class="col s6 m6 l6">
+                    <a class="waves-effect waves-light btn-large grey">lihat materi</a>
+                </div>
+            </div>
         </div>
         <div class="col s12 m6 l6">
             <div class="center" style="padding:30px 0 30px 0;">
-                <img style="width:60%;" src="https://kelastutorial.com/images/html-logo.png"/>
+                <img style="width:60%;" alt="logo html" title="logo html" src="https://kelastutorial.com/images/html-logo.png"/>
             </div>
             <span class="tutorials-index-category">LEVEL</span><br/>
-            <span>PEMULA</span><br/><br/>
+            <span style="font-weight:600;color:#444;">PEMULA</span><br/><br/>
             <span class="tutorials-index-category">KLASIFIKASI</span><br/>
-            <span>PENGEMBANGAN WEB</span>
+            <span style="font-weight:600;color:#444;">PENGEMBANGAN WEB</span>
         </div>
     </div>
 </div>
