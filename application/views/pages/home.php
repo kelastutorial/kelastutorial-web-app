2f8328f6e4c1698fd817814119b0d7ee74529862
purwanto1337 <purwanto1337@gmail.com>
2017-03-15 17:32:45 +0000

    update html page

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,5 +1,5 @@
         <div class="wall-home card">
-            <img class="homepage-header-logo" src="<?php echo site_url('images/kelastutorial-logo-white.svg'); ?>"/>
+            <img alt="logo kelastutorial" class="homepage-header-logo" src="<?php echo site_url('images/kelastutorial-logo-white.svg'); ?>"/>
             <form onsubmit="location.href='https://kelastutorial.com/search?q=' + document.getElementById('searchInput').value; return false;">
                 <input id="searchInput" class="hoverable" style="border-radius:2px;background-color:#fff;padding:0 30px 0 30px;font-size:15px;width:50%" type="text" placeholder="Cari Tutorial"/>
             </form>
@@ -8,27 +8,27 @@
             <h3>Tutorial Terpopuler</h3>
             <div class="row">
                 <div class="col s6 m4 l2">
-                    <a href="<?php echo site_url('html'); ?>"><img src="<?php echo site_url('images/html-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
+                    <a href="<?php echo site_url('html'); ?>"><img alt="icon html" src="<?php echo site_url('images/html-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
                     HTML</a><br/><br/><br/>
                 </div>
                 <div class="col s6 m4 l2">
-                    <a href="<?php echo site_url('java'); ?>"><img src="<?php echo site_url('images/java-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
+                    <a href="<?php echo site_url('java'); ?>"><img alt="icon java"  src="<?php echo site_url('images/java-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
                     Java</a><br/><br/><br/>
                 </div>
                 <div class="col s6 m4 l2">
-                    <a href="<?php echo site_url('python'); ?>"><img src="<?php echo site_url('images/python-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
+                    <a href="<?php echo site_url('python'); ?>"><img alt="icon python"  src="<?php echo site_url('images/python-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
                     Python</a><br/><br/><br/>
                 </div>
                 <div class="col s6 m4 l2">
-                    <a href="<?php echo site_url('json'); ?>"><img src="<?php echo site_url('images/json-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
+                    <a href="<?php echo site_url('json'); ?>"><img alt="icon json"  src="<?php echo site_url('images/json-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
                     JSON</a><br/><br/><br/>
                 </div>
                 <div class="col s6 m4 l2">
-                    <a href="<?php echo site_url('git'); ?>"><img src="<?php echo site_url('images/git-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
+                    <a href="<?php echo site_url('git'); ?>"><img alt="icon git"  src="<?php echo site_url('images/git-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
                     Git</a><br/><br/><br/>
                 </div>
                 <div class="col s6 m4 l2">
-                    <a href="<?php echo site_url('tutorials'); ?>"><img src="<?php echo site_url('images/more-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
+                    <a href="<?php echo site_url('tutorials'); ?>"><img alt="icon more"  src="<?php echo site_url('images/more-icon.png'); ?>" style="height:50px;margin-bottom:7px;"/><br/>
                     Semua Tutorial</a><br/><br/><br/>
                 </div>
             </div>
--- application/views/tutorials/html/left_sidebar.php
+++ application/views/tutorials/html/left_sidebar.php
@@ -36,7 +36,7 @@
                 <div class="mobile-navigation">
                     <ul class="side-nav" id="mobile-demo" style="width:70%;z-index:1000;">
                         <div class="header-mobile grey lighten-3" style="height:110px;padding:33px 5px 0 0;text-align:center">
-                            <a href="<?php echo site_url(); ?>"><img src="<?php echo site_url('images/kelastutorial-logo.svg'); ?>" height="38px"/></a>
+                            <a href="<?php echo site_url(); ?>"><img alt="logo-kelastutorial" src="<?php echo site_url('images/kelastutorial-logo.svg'); ?>" height="38px"/></a>
                         </div>
                         <li style="border-bottom:1px solid #e0e0e0"><a href="<?php echo site_url('tutorials'); ?>">Semua Tutorial</a></li>
                         <ul class='collapsible collapsible-accordion'>    
