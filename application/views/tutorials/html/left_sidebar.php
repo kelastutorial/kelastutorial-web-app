480b9026e4ae977c455fc9fd61cf5257eccae6a1
purwanto1337 <purwanto1337@gmail.com>
2017-03-15 17:26:24 +0000

    update html page

--- application/views/tutorials/html/left_sidebar.php
+++ application/views/tutorials/html/left_sidebar.php
@@ -1,84 +1,83 @@
-<div class="tutorial-wall"></div>
-<div class="container" style="height:auto;width:100%;padding-top:30px;">
-    <div class="row">
-        <div class="col s12 m12 l3 hide-on-tablet">
-                <div class="left-sidebar-container">
-            <div class="grey lighten-3 left-sidebar">
-                <span class="subtutorial">Pendahuluan</span><br/>
-                <a href="" class="subtutorial-list">HTML - Pengertian</a><br/>
-                <a href="" class="subtutorial-list">HTML - Sejarah</a><br/>
-                <a href="" class="subtutorial-list">HTML - Versi</a><br/>
-            </div>
-            <div class="grey lighten-3 left-sidebar">
-                <span class="subtutorial">HTML Dasar</span><br/>
-                <a href="" class="subtutorial-list">HTML - Syntax Dasar</a><br/>
-                <a href="" class="subtutorial-list">HTML - Elemen</a><br/>
-                <a href="" class="subtutorial-list">HTML - Atribut</a><br/>
-                <a href="" class="subtutorial-list">HTML - Atribut Khusus</a><br/>
-                <a href="" class="subtutorial-list">HTML - Event</a><br/>
-                <a href="" class="subtutorial-list">HTML - Form</a><br/>
-                <a href="" class="subtutorial-list">HTML - SVG</a><br/>
-                <a href="" class="subtutorial-list">HTML - Layout</a><br/>
-                <a href="" class="subtutorial-list">HTML - Audio</a><br/>
-                <a href="" class="subtutorial-list">HTML - Video</a><br/>
-            </div>
-            <div class="grey lighten-3 left-sidebar">
-                <span class="subtutorial">HTML Tingkat Lanjut</span><br/>
-                <a href="" class="subtutorial-list">HTML - Geolocation</a><br/>
-                <a href="" class="subtutorial-list">HTML - Microdata</a><br/>
-                <a href="" class="subtutorial-list">HTML - Drag &amp; Drop</a><br/>
-                <a href="" class="subtutorial-list">HTML - Layout Responsive</a><br/>
-            </div>
+
+        <div class="tutorial-wall"></div>
+        <div class="container" style="height:auto;width:100%;padding-top:30px;">
+            <div class="row">
+                <div class="col s12 m12 l3 hide-on-tablet">
+                    <div class="left-sidebar-container">
+                        <div class="grey lighten-3 left-sidebar">
+                            <span class="subtutorial">Pendahuluan</span><br/>
+                            <a href="" class="subtutorial-list">HTML - Pengertian</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Sejarah</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Versi</a><br/>
+                        </div>
+                        <div class="grey lighten-3 left-sidebar">
+                            <span class="subtutorial">HTML Dasar</span><br/>
+                            <a href="" class="subtutorial-list">HTML - Syntax Dasar</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Elemen</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Atribut</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Atribut Khusus</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Event</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Form</a><br/>
+                            <a href="" class="subtutorial-list">HTML - SVG</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Layout</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Audio</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Video</a><br/>
+                        </div>
+                        <div class="grey lighten-3 left-sidebar">
+                            <span class="subtutorial">HTML Tingkat Lanjut</span><br/>
+                            <a href="" class="subtutorial-list">HTML - Geolocation</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Microdata</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Drag &amp; Drop</a><br/>
+                            <a href="" class="subtutorial-list">HTML - Layout Responsive</a><br/>
+                        </div>
+                    </div>
                 </div>
-        </div>
-        
-        <div class="mobile-navigation">
-            <ul class="side-nav" id="mobile-demo" style="width:70%;z-index:1000;">
-                <div class="header-mobile grey lighten-3" style="height:110px;padding:33px 5px 0 0;text-align:center">
-                    <img src="<?php echo site_url('images/kelastutorial-logo.svg'); ?>" height="38px"/>
-                </div>
-                <li style="border-bottom:1px solid #e0e0e0"><a href="<?php echo site_url('tutorials'); ?>">Semua Tutorial</a></li>
-                
-                <ul class='collapsible collapsible-accordion'> 
-                    
-                <li><a class='collapsible-header waves-effect waves-yellow lighten-3' style="font-weight:500">Pendahuluan</a>
-                    <div class='collapsible-body'>
-                    <ul>
-                    <li><a href='#'>HTML - Pengertian</a></li>
-                    <li><a href='#'>HTML - Sejarah</a></li>
-                    <li><a href='#'>HTML - Versi</a></li>
+                <!-- Mobile Navigation -->
+                <div class="mobile-navigation">
+                    <ul class="side-nav" id="mobile-demo" style="width:70%;z-index:1000;">
+                        <div class="header-mobile grey lighten-3" style="height:110px;padding:33px 5px 0 0;text-align:center">
+                            <a href="<?php echo site_url(); ?>"><img src="<?php echo site_url('images/kelastutorial-logo.svg'); ?>" height="38px"/></a>
+                        </div>
+                        <li style="border-bottom:1px solid #e0e0e0"><a href="<?php echo site_url('tutorials'); ?>">Semua Tutorial</a></li>
+                        <ul class='collapsible collapsible-accordion'>    
+                            <li>
+                                <a class='collapsible-header waves-effect waves-red lighten-3' style="font-weight:500">Pendahuluan</a>
+                                <div class='collapsible-body'>
+                                    <ul>
+                                        <li><a href='#'>HTML - Pengertian</a></li>
+                                        <li><a href='#'>HTML - Sejarah</a></li>
+                                        <li><a href='#'>HTML - Versi</a></li>
+                                    </ul>
+                                </div>
+                            </li> 
+                            <li>
+                                <a class='collapsible-header waves-effect waves-red lighten-3' style="font-weight:500">HTML Dasar</a>
+                                <div class='collapsible-body'>
+                                    <ul>
+                                        <li><a href='#'>HTML - Syntax Dasar</a></li>
+                                        <li><a href='#'>HTML - Elemen</a></li>
+                                        <li><a href='#'>HTML - Atribut</a></li>
+                                        <li><a href='#'>HTML - Atribut Khusus</a></li>
+                                        <li><a href='#'>HTML - Event</a></li>
+                                        <li><a href='#'>HTML - Form</a></li>
+                                        <li><a href='#'>HTML - SVG</a></li>
+                                        <li><a href='#'>HTML - Layout</a></li>
+                                        <li><a href='#'>HTML - Audio</a></li>
+                                        <li><a href='#'>HTML - Video</a></li>
+                                    </ul>
+                                </div>
+                            </li> 
+                            <li>
+                                <a class='collapsible-header waves-effect waves-red lighten-3' style="font-weight:500">HTML Tingkat Lanjut</a>
+                                <div class='collapsible-body'>
+                                    <ul>
+                                        <li><a href='#'>HTML - Geolocation</a></li>
+                                        <li><a href='#'>HTML - Microdata</a></li>
+                                        <li><a href='#'>HTML - Drag &amp; Drop</a></li>
+                                        <li><a href='#'>HTML - Layout Responsive</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
                     </ul>
-                    </div>
-                </li>
-                    
-                <li><a class='collapsible-header waves-effect waves-yellow lighten-3' style="font-weight:500">HTML Dasar</a>
-                    <div class='collapsible-body'>
-                    <ul>
-                    <li><a href='#'>HTML - Syntax Dasar</a></li>
-                    <li><a href='#'>HTML - Elemen</a></li>
-                    <li><a href='#'>HTML - Atribut</a></li>
-                    <li><a href='#'>HTML - Atribut Khusus</a></li>
-                    <li><a href='#'>HTML - Event</a></li>
-                    <li><a href='#'>HTML - Form</a></li>
-                    <li><a href='#'>HTML - SVG</a></li>
-                    <li><a href='#'>HTML - Layout</a></li>
-                    <li><a href='#'>HTML - Audio</a></li>
-                    <li><a href='#'>HTML - Video</a></li>
-                    </ul>
-                    </div>
-                </li>
-                    
-                <li><a class='collapsible-header waves-effect waves-yellow lighten-3' style="font-weight:500">HTML Tingkat Lanjut</a>
-                    <div class='collapsible-body'>
-                    <ul>
-                    <li><a href='#'>HTML - Geolocation</a></li>
-                    <li><a href='#'>HTML - Microdata</a></li>
-                    <li><a href='#'>HTML - Drag &amp; Drop</a></li>
-                    <li><a href='#'>HTML - Layout Responsive</a></li>
-                    </ul>
-                    </div>
-                </li>
-                
-                </ul>
-        </ul>
-     </div>
+                </div><!-- End Mobile Navigation -->
