976541d01a8368d03da006b552cf418b78938d26
purwanto1337 <purwanto1337@gmail.com>
2017-03-16 17:48:19 +0000

    update java tutorial landing page

--- application/views/tutorials/java/index.php
+++ application/views/tutorials/java/index.php
@@ -1,22 +1,22 @@
         
         <div style="background-color:#fafbfc;t">
             <div class="container center" style="padding:150px 0 130px 0;">
-                <h2 class="tutorials-index-head-two">Tutorial Java</h2>
-                <p>Pelajari kemampuan dasar untuk membuat halaman atau aplikasi website modern dengan HTML.</p>
+                <h2 class="tutorials-index-head-two">Tutorial Bahasa Pemrograman Java</h2>
+                <p>Pelajari bahasa pemrograman komputer berorientasi objek Java</p>
             </div>
         </div>
         <div class="container about-this-tutorial-container">
             <div class="row">
                 <div class="col s12 m6 l6">
                     <div class="about-this-tutorial">
-                        <h4 class="tutorials-index-head-four" style="color:#0375BC;">Tentang Bahasa HTML</h4>
-                        <p>HyperText Markup Language (HTML) adalah sebuah standar bahasa markup untuk membuat sebuah halaman web atau aplikasi web. HTML biasanya di kombinasikan dengan Cascading Style Sheets (CSS) dan Javascript agar halaman atau aplikasi website menjadi lebih menarik dan berguna. Dokumen HTML bekerja dengan sangat sederhana, web browser menerima dokumen HTML dari webserver atau dari penyimpanan lokal kemudian me-rendernya menjadi halaman website yang diiginkan.</p>
-                        <p>Pada tutorial ini Anda akan mempelajari bagaimana cara membuat halaman website statis dengan HTML. Anda akan mulai belajar dari syntax dasar, elemen-elemen HTML, layout dan pengetahuan dasar lainya tentang HTML.</p>
-                        <p>Anda juga akan mempelajari bagaimana membuat website yang responsive, dimana halaman website akan tetap terlihat bagus saat dilihat di Komputer Desktop maupun di Tablet atau di Smartphone</p>
+                        <h4 class="tutorials-index-head-four" style="color:#0375BC;">Tentang Bahasa Pemrograman Java</h4>
+                        <p>Java adalah bahasa pemrograman komputer umum "general-purpose" yang berbasis class dan berorientasi objek dan didesain spesial untuk memiliki beberapa implementasi dependensi sebisa mungkin. Hal itu dimaksudkan agar pengembang aplikasi Java menggunakan konsep "write once, run anywhere" (WORA) tulis sekali, jalankan dimana saja. Hal ini dikarenakan Java dijalankan menggunakan Java Virtual Machine (JVM) yang berarti kode Java di semua arsitektur adalah sama.</p>
+                        <p>Pada tutorial ini Anda akan mempelajari penggunaan bahasa pemrograman Java secara mendasar. Anda akan mulai belajar dari syntax dasar, tipe data, variabel, encapsulation sampai dengan level mahir.</p>
+                        <p>Anda juga akan mempelajari bagaimana menggunakan bahasa pemrograman Java untuk mengirim email, networking, multithreading, dan lain-lain.</p>
                         <div class="row" style="margin-top:50px;">
-                            <p style="margin-left: 11px;color: #777;font-style: italic;font-size: 14px;">Pelajari HTML sekarang dan mulai membuat website yang berguna.</p>
+                            <p style="margin-left: 11px;color: #777;font-style: italic;font-size: 14px;">Pelajari bahasa pemrograman berorientasi objek Java dan mulai membuat aplikasi yang berguna.</p>
                             <div class="col s12 m12 l6">
-                                <a href="./java/pengertian-java.java" style="width:100%;margin-bottom:13px;background-color:#0375BC;" class="waves-effect waves-light btn-large"><i class="material-icons right">school</i>pelajari</a>
+                                <a href="./java/pengertian-java.html" style="width:100%;margin-bottom:13px;background-color:#0375BC;" class="waves-effect waves-light btn-large"><i class="material-icons right">school</i>pelajari</a>
                             </div>
                             <div class="col s12 m12 l6">
                                 <a href="#java-syllabus" style="width:100%" class="waves-effect waves-light btn-large grey lighten-1">lihat materi</a>
@@ -31,7 +31,7 @@
                     <span class="tutorials-index-category">LEVEL</span><br/>
                     <span style="font-weight:600;color:#444;">PEMULA</span><br/><br/>
                     <span class="tutorials-index-category">KLASIFIKASI</span><br/>
-                    <span style="font-weight:600;color:#444;">PENGEMBANGAN WEB</span>
+                    <span style="font-weight:600;color:#444;">BAHASA PEMROGRAMAN</span>
                 </div>
             </div>
         </div>
@@ -46,9 +46,12 @@
                                 <div class="row syllabus-content">
                                     <div class="col s12 m6 l6">
                                         <ol type="i">
-                                            <li><a href="./java/pengertian-java.java">Pengertian HTML</a></li>
-                                            <li><a href="#">Sejarah HTML</a></li>
-                                            <li><a href="#">Versi HTML</a></li>
+                                            <li><a href="./java/pengertian-java.java">Pengertian Java</a></li>
+                                            <li><a href="#">Sejarah Java</a></li>
+                                            <li><a href="#">Versi Java</a></li>
+                                            <li><a href="#">Instalasi Java</a></li>
+                                            <li><a href="#">Menjalankan Java</a></li>
+                                            <li><a href="#">IDE Java</a></li>
                                         </ol>
                                     </div>
                                     <div class="col s12 m6 l6">
@@ -58,26 +61,59 @@
                             </div>
                         </li>
                         <li>
-                            <div class="collapsible-header syllabus-header"><i class="fa fa-angle-down" aria-hidden="true"></i>HTML Dasar</div>
+                            <div class="collapsible-header syllabus-header"><i class="fa fa-angle-down" aria-hidden="true"></i>Java Dasar</div>
                             <div class="collapsible-body">
                                 <div class="row syllabus-content">
                                     <div class="col s6 m6 l6">
                                         <ol type="i">
-                                            <li><a href="#">Syntax Dasar HTML</a></li>
-                                            <li><a href="#">Elemen HTML</a></li>
-                                            <li><a href="#">Atribut HTML</a></li>
-                                            <li><a href="#">Atribut Khusus HTML</a></li>
-                                            <li><a href="#">Event HTML</a></li>
+                                            <li><a href="#">Syntax Dasar Java</a></li>
+                                            <li><a href="#">Object dan Class</a></li>
+                                            <li><a href="#">Tipe Data Java</a></li>
+                                            <li><a href="#">Variabel Java</a></li>
+                                            <li><a href="#">Modifier Java</a></li>
+                                            <li><a href="#">Operator Dasar Java</a></li>
+                                            <li><a href="#">Loop Control Java</a></li>
+                                            <li><a href="#">If Else Java</a></li>
+                                            <li><a href="#">Nested If Java</a></li>
+                                            <li><a href="#">Switch Java</a></li>
                                         </ol>
                                     </div>
                                     <div class="col s6 m6 l6">
                                         <ol type="i">
-                                            <li><a href="#">Form HTML</a></li>
-                                            <li><a href="#">SVG HTML</a></li>
-                                            <li><a href="#">Layout HTML</a></li>
-                                            <li><a href="#">Audio HTML</a></li>
-                                            <li><a href="#">Video HTML</a></li>
+                                            <li><a href="#">Number Java</a></li>
+                                            <li><a href="#">Characters Java</a></li>
+                                            <li><a href="#">Strings Java</a></li>
+                                            <li><a href="#">Arrays Java</a></li>
+                                            <li><a href="#">Waktu &amp; Tanggal</a></li>
+                                            <li><a href="#">Regular Expressions Java</a></li>
+                                            <li><a href="#">Methods Java</a></li>
+                                            <li><a href="#">Files I/O Java</a></li>
+                                            <li><a href="#">Exceptions Java</a></li>
+                                            <li><a href="#">Inner Class Java</a></li>
                                         </ol>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="collapsible-header syllabus-header"><i class="fa fa-angle-down" aria-hidden="true"></i>HTML Tingkat Menengah</div>
+                            <div class="collapsible-body">
+                                <div class="row syllabus-content">
+                                    <div class="col s6 m6 l6">
+                                        <ol type="i">
+                                            <li><a href="#">Inheritance Java</a></li>
+                                            <li><a href="#">Overriding Java</a></li>
+                                            <li><a href="#">Polymorphism Java</a></li>
+                                            <li><a href="#">Abstraction Java</a></li>
+                                        </ol>
+                                    </div>
+                                    <div class="col s6 m6 l6">
+                                        <ol type="i">
+                                            <li><a href="#">Encapsulation Java</a></li>
+                                            <li><a href="#">Interfaces Java</a></li>
+                                            <li><a href="#">Packages Java</a></li>
+                                        </ol>
+                                      
                                     </div>
                                 </div>
                             </div>
@@ -88,14 +124,19 @@
                                 <div class="row syllabus-content">
                                     <div class="col s6 m6 l6">
                                         <ol type="i">
-                                            <li><a href="#">Geolocation HTML</a></li>
-                                            <li><a href="#">Microdata HTML</a></li>
-                                            <li><a href="#">Drag &amp; Drop HTML</a></li>
-                                            <li><a href="#">Layout Responsive HTML</a></li>
+                                            <li><a href="#">Data Struktur Java</a></li>
+                                            <li><a href="#">Collections Java</a></li>
+                                            <li><a href="#">Generics Java</a></li>
+                                            <li><a href="#">Serialization Java</a></li>
                                         </ol>
                                     </div>
                                     <div class="col s6 m6 l6">
-                                      
+                                        <ol type="i">
+                                            <li><a href="#">Networking Java</a></li>
+                                            <li><a href="#">Email Dengan Java</a></li>
+                                            <li><a href="#">Multithreading Java</a></li>
+                                            <li><a href="#">Applet Dasar Java</a></li>
+                                        </ol>
                                     </div>
                                 </div>
                             </div>
@@ -111,15 +152,16 @@
                     <p style="margin-top:35px;">Untuk dapat dengan cepat mengikuti tutorial ini, Anda disarankan untuk menguasai terlebih dahulu beberapa persyaratan minimal dibawah ini :</p>
                     <p>
                         <ol type="i">
-                            <li>Aktif dan menyukai dunia internet</li>
-                            <li>Menguasai dasar jaringan komputer</li>
-                            <li>Menguasai pengetahuan dasar internet</li>
+                            <li>Mengetahui bagaimana komputer bekerja</li>
+                            <li>Mengetahui dasar jaringan komputer</li>
+                            <li>Mengetahui dasar algoritma dan pemrograman</li>
                         </ol>
                     </p>
+                    <p>Anda tidak harus berpengalaman di bahasa pemrograman untuk mengikuti tutorial. Level pemula juga sangat disarankan mengikuti tutorial ini.</p>
                     <div class="row" style="margin-top:110px;">
-                        <p style="margin-left: 11px;color: #777;font-style: italic;font-size: 14px;">Pelajari HTML sekarang dan mulai membuat website yang berguna.</p>
+                        <p style="margin-left: 11px;color: #777;font-style: italic;font-size: 14px;">Pelajari bahasa pemrograman berorientasi objek Java dan mulai membuat aplikasi yang berguna.</p>
                         <div class="col s12 m6 l6">
-                            <a href="./java/pengertian-java.java" style="width:100%;margin-bottom:13px;background-color:#0375BC;" class="waves-effect waves-light btn-large "><i class="material-icons right">school</i>pelajari</a>
+                            <a href="./java/pengertian-java.html" style="width:100%;margin-bottom:13px;background-color:#0375BC;" class="waves-effect waves-light btn-large "><i class="material-icons right">school</i>pelajari</a>
                         </div>
                         <div class="col s12 m6 l6">
                             <a href="#java-syllabus" style="width:100%" class="waves-effect waves-light btn-large grey lighten-1">lihat materi</a>
