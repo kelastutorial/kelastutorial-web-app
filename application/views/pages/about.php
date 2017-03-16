a9bf6d8284da8a1e90a48c49ef229f7e7b0d410a
purwanto1337 <purwanto1337@gmail.com>
2017-03-16 16:08:48 +0000

    update mobile navigation

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -96,3 +96,20 @@
             <span>Jika Anda mempunyai pertanyaan, kami mempunyai jawaban.<br/>Kami akan selalu membantu Anda. Hubungi kami kapan saja.</span><br/><br/>
             <a href="mailto:support@kelastutorial.com" class=" white waves-effect waves-light btn-large red-text hoverable">HUBUNGI KAMI SEKARANG</a></a>
         </div>
+        <!-- Mobile Navigation -->
+        <div class="mobile-navigation">
+            <ul class="side-nav" id="mobile-demo" style="width:70%;z-index:1000;">
+                <div class="header-mobile grey lighten-3" style="height:110px;padding:33px 5px 0 0;text-align:center">
+                    <a href="<?php echo site_url(); ?>"><img alt="logo-kelastutorial" src="<?php echo site_url('images/kelastutorial-logo.svg'); ?>" height="38px"/></a>
+                </div>
+                <li style="border-bottom:1px solid #e0e0e0"><a href="<?php echo site_url('tutorials'); ?>">Semua Tutorial</a></li>
+                <li><a href="<?php echo site_url('about'); ?>">Tentang Kami</a></li>
+                <li><a href="<?php echo site_url('site-terms'); ?>">Syarat dan Ketentuan</a></li>
+                <li><a href="<?php echo site_url('privacy-policy'); ?>">Kebijakan Privasi</a></li>
+                <li><a href="<?php echo site_url('mailto:support@kelastutorial.com'); ?>">Hubungi Kami</a></li>
+                <li><a href="<?php echo site_url('program'); ?>">Program</a></li>
+                <li><a href="<?php echo site_url('partner'); ?>">Partner</a></li>
+                <li><a href="<?php echo site_url('reference'); ?>">Referensi</a></li>
+                <li><a href="<?php echo site_url('repport-issue'); ?>">Laporkan Isu</a></li>
+            </ul>
+        </div><!-- End Mobile Navigation -->
