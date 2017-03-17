d11ff3f4401e3859505823ccbef77c2bcb9ccbb0
purwanto1337 <purwanto1337@gmail.com>
2017-03-17 16:59:44 +0700

    update python tutorial landing page

--- application/views/tutorials/json/index.php
+++ application/views/tutorials/json/index.php
@@ -2,20 +2,20 @@
         <div style="background-color:#fafbfc;t">
             <div class="container center" style="padding:150px 0 130px 0;">
                 <h2 class="tutorials-index-head-two">Tutorial JSON</h2>
-                <p>Pelajari format pertukaran data yang ringan, mudah dibaca dan ditulis oleh manusia, serta mudah diterjemahkan dan dibuat (generate) oleh komputer</p>
+                <p>Pelajari format pertukaran data yang ringan, mudah dibaca dan ditulis oleh manusia, serta mudah diterjemahkan dan dibuat (generate) oleh komputer.</p>
             </div>
         </div>
         <div class="container about-this-tutorial-container">
             <div class="row">
                 <div class="col s12 m6 l6">
                     <div class="about-this-tutorial">
-                        <h4 class="tutorials-index-head-four" style="color:#E44D26;">Tentang Bahasa HTML</h4>
+                        <h4 class="tutorials-index-head-four" style="color:#444;">Tentang Format JSON</h4>
                         <p>JSON (JavaScript Object Notation) adalah format pertukaran data yang ringan, mudah dibaca dan ditulis oleh manusia, serta mudah diterjemahkan dan dibuat (generate) oleh komputer. Format ini dibuat berdasarkan bagian dari Bahasa Pemprograman JavaScript, Standar ECMA-262 Edisi ke-3 - Desember 1999. JSON merupakan format teks yang tidak bergantung pada bahasa pemprograman apapun karena menggunakan gaya bahasa yang umum digunakan oleh programmer keluarga C termasuk C, C++, C#, Java, JavaScript, Perl, Python dll. Oleh karena sifat-sifat tersebut, menjadikan JSON ideal sebagai bahasa pertukaran-data.</p>
                         <p>Pada tutorial ini Anda akan mempelajari penggunaan JSON untuk mengembangkan aplikasi dengan format pertukaran data. Setelah mengikuti tutorial ini, Anda diharapkan dapat menguasai dan memahami dasar penggunaan JSON dan bagaimana menerapkanya.</p>
                         <div class="row" style="margin-top:50px;">
                             <p style="margin-left: 11px;color: #777;font-style: italic;font-size: 14px;">Pelajari JSON untuk format pertukaran data yang ringan, mudah dibaca.</p>
                             <div class="col s12 m12 l6">
-                                <a href="./json/pengertian-json.html" style="width:100%;margin-bottom:13px;" class="waves-effect waves-light btn-large  grey darken-2"><i class="material-icons right">school</i>pelajari</a>
+                                <a href="./json/pengertian-json.html" style="width:100%;margin-bottom:13px;" class="waves-effect waves-light btn-large  grey darken-4"><i class="material-icons right">school</i>pelajari</a>
                             </div>
                             <div class="col s12 m12 l6">
                                 <a href="#json-syllabus" style="width:100%" class="waves-effect waves-light btn-large grey lighten-1">lihat materi</a>
@@ -25,7 +25,7 @@
                 </div>
                 <div class="col s12 m6 l6">
                     <div class="center" style="padding:30px 0 30px 0;">
-                        <img style="width:60%;" alt="logo json" title="logo json" src="https://kelastutorial.com/images/json-logo.png"/>
+                        <img style="width:90%;" alt="logo json" title="logo json" src="https://kelastutorial.com/images/logo-json.gif"/>
                     </div>
                     <span class="tutorials-index-category">LEVEL</span><br/>
                     <span style="font-weight:600;color:#444;">MENENGAH</span><br/><br/>
@@ -36,7 +36,7 @@
         </div>
         <div style="background-color: #FAF0E6;" class="syllabus-tutorial-container" id="json-syllabus">
             <div class="container">
-                <h4 class="tutorials-index-head-four" style="color:#fff;text-align:center">Apa yang akan Anda pelajari ?</h4>
+                <h4 class="tutorials-index-head-four" style="color:#444;text-align:center">Apa yang akan Anda pelajari ?</h4>
                 <div style="max-width:850px;padding:20px 0 20px 0;margin:0 auto;">
                     <ul class="collapsible white" data-collapsible="expandable">
                         <li>
@@ -91,7 +91,7 @@
                     <div class="row" style="margin-top:110px;">
                         <p style="margin-left: 11px;color: #777;font-style: italic;font-size: 14px;">Pelajari JSON untuk format pertukaran data yang ringan, mudah dibaca.</p>
                         <div class="col s12 m6 l6">
-                            <a href="./json/pengertian-json.html" style="width:100%;margin-bottom:13px;" class="waves-effect waves-light btn-large  grey darken-2"><i class="material-icons right">school</i>pelajari</a>
+                            <a href="./json/pengertian-json.html" style="width:100%;margin-bottom:13px;" class="waves-effect waves-light btn-large  grey darken-4"><i class="material-icons right">school</i>pelajari</a>
                         </div>
                         <div class="col s12 m6 l6">
                             <a href="#json-syllabus" style="width:100%" class="waves-effect waves-light btn-large grey lighten-1">lihat materi</a>
