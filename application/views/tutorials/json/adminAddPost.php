3ca0231535c87efe5ddb114b07f72fa1702f1892
purwanto1337 <purwanto1337@gmail.com>
2017-04-09 04:59:33 +0700

    update git tutorial

--- application/views/tutorials/json/adminAddPost.php
+++ application/views/tutorials/json/adminAddPost.php
@@ -3,14 +3,12 @@
 <?php echo form_open('json/adminAddPost'); ?>
     <label for="title">Title</label><br/>
     <input type="text" name="title"/><br/>
-
     <label for="content">Content</label><br/>
     <textarea name="content" rows="50"cols="150"></textarea><br/>
-
     <label for="meta_desc">Meta Description</label><br/>
     <input type="hidden" name="published" value="<?php echo date('Y-m-d'); ?>" readonly="readonly">
     <input type="hidden" name="last_update" value="<?php echo date('Y-m-d'); ?>" readonly="readonly">
-    <textarea name="meta_desc"></textarea><br/>
+    <textarea name="meta_desc" rows="20"cols="150"></textarea><br/>
     <input type="submit" name="submit" value="submit"/><br/>
 
 <?php echo form_close(); ?>
